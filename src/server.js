c8895d072c5fdf0fe0fe7992fde262487a2dd635
anon <test@test.com>
2026-03-04 14:18:49 +0800

    webid: raise request body limits for selfie/id upload payloads

--- src/server.js
+++ src/server.js
@@ -34,8 +34,8 @@ app.use(
   })
 );
 app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
+app.use(express.json({ limit: '25mb' }));
 app.use(
   session({
     name: 'ha.sid',
